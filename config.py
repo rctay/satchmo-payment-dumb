2996ab3232c24ccbdc55d2da772617b17fe9e18c
Tay Ray Chuan <rctay89@gmail.com>
2010-03-17 22:37:35 +0800

    add basic config and processor

---
+++ config.py
@@ -0,0 +1,43 @@
+from livesettings import *
+from django.utils.translation import ugettext_lazy as _
+
+PAYMENT_GROUP = ConfigurationGroup('PAYMENT_DUMB',
+    _('Dumb Payment Module Settings'),
+    ordering = 200)
+
+config_register_list(
+    BooleanValue(PAYMENT_GROUP,
+        'LIVE',
+        description=_("Accept real payments"),
+        help_text=_("False if you want to be in test mode"),
+        default=False),
+
+    BooleanValue(PAYMENT_GROUP,
+        'SSL',
+        description=_("Use SSL for the module checkout pages?"),
+        default=False),
+
+    ModuleValue(PAYMENT_GROUP,
+        'MODULE',
+        description=_('Implementation module'),
+        hidden=True,
+        default = 'satchmo_payment_dumb'),
+
+    StringValue(PAYMENT_GROUP,
+        'KEY',
+        description=_("Module key"),
+        hidden=True,
+        default = 'DUMB'),
+
+    StringValue(PAYMENT_GROUP,
+        'LABEL',
+        description=_('English name for this group on the checkout screens'),
+        default = 'Pay Now',
+        help_text = _('This will be passed to the translation utility')),
+
+    StringValue(PAYMENT_GROUP,
+        'URL_BASE',
+        description=_('The url base used for constructing urlpatterns which will use this module'),
+        help_text = _('Please do not add the \'^\' or trailing \'/\'; this is done for you.'),
+        default = 'dumb'),
+)
---
+++ processor.py
@@ -0,0 +1,5 @@
+from payment.modules.base import HeadlessPaymentProcessor
+
+class PaymentProcessor(HeadlessPaymentProcessor):
+    def __init__(self, settings):
+        super(PaymentProcessor, self).__init__('dumb', settings)
