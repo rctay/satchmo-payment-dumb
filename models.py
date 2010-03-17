a512799f464636ce628c9c94f6ed01bed1260d97
Tay Ray Chuan <rctay89@gmail.com>
2010-03-18 01:46:30 +0800

    doc why we didn't define PAYMENT_PROCESSOR

--- models.py
+++ models.py
@@ -1,1 +1,7 @@
+"""
+Unlike the built-in satchmo payment modules, we're not defining
+`PAYMENT_PROCESSOR`; satchmo makes a lot of assumptions about modules with that
+attribute that we, as a module outside of the `payment.modules` namespace, can't
+satisfy.
+"""
 import config
