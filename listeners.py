70f0867a8b4018e4c2571d33ce6e6d63cb578140
Tay Ray Chuan <rctay89@gmail.com>
2010-03-18 00:58:19 +0800

    hook onto signal and display manual method

--- listeners.py
+++ listeners.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import url, include
+from django.utils.translation import ugettext_lazy as _
 from livesettings import config_get_group, config_value
+from payment.signals import payment_choices
 from satchmo_store import shop
 from signals_ahoy.signals import collect_urls
 
@@ -12,3 +14,23 @@ def add_dumb_urls(sender, patterns=None, **kwargs):
     )
 
 collect_urls.connect(add_dumb_urls, sender=shop)
+
+def add_dumb_method(sender, choices=None, **kwargs):
+    """
+    The `payment_choices` signal doesn't really allow us cover all the bases -
+    `payment.active_gateways()` is the one to hack.
+
+    But it's close.
+    """
+    if not choices:
+        return
+
+    module = config_value(config_group, 'MODULE')
+    group = 'PAYMENT_DUMB'
+
+    # repeat logic in `payment.config.labelled_gateway_choices()`
+    defaultlabel = module.__name__.split('.')[-1]
+    label = _(config_value(group, 'LABEL', default = defaultlabel))
+    choices.append((group, label))
+
+payment_choices.connect(add_dumb_method)
