b112efe17cad9f8b5ddb37c193dca414c9568794
Tay Ray Chuan <rctay89@gmail.com>
2010-03-18 19:23:22 +0800

    processor: implement capture_payment()
    We do nothing but return success.
    
    This traps the 'bla bla not implemented' error.

--- processor.py
+++ processor.py
@@ -1,5 +1,22 @@
-from payment.modules.base import HeadlessPaymentProcessor
+from decimal import Decimal
+
+from django.utils.translation import ugettext_lazy as _
+from livesettings import config_value, config_get_group
+from payment.modules.base import HeadlessPaymentProcessor, ProcessorResult
+
+config = config_get_group('PAYMENT_DUMB')
 
 class PaymentProcessor(HeadlessPaymentProcessor):
     def __init__(self, settings):
         super(PaymentProcessor, self).__init__('dumb', settings)
+
+    def capture_payment(self, testing=False, order=None, amount=Decimal('0')):
+        """
+        Users are expected to implement recording of payments, so we just return
+        a blanket success.
+        """
+        if not config_value(config, 'LIVE'):
+            payment = self.record_payment(order=order, amount=amount,
+                transaction_id="TESTING", reason_code='0')
+
+        return ProcessorResult(self.key, True, _('Skipping payment recording'))
