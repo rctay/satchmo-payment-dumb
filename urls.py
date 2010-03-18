9682f93ce24df3604a14420d48f7120986f837b9
Tay Ray Chuan <rctay89@gmail.com>
2010-03-18 16:30:27 +0800

    urls: replace references to 'dumb' with config accesses

--- urls.py
+++ urls.py
@@ -2,12 +2,14 @@ from django.conf.urls.defaults import *
 from livesettings import config_value, config_get_group
 
 config = config_get_group('PAYMENT_DUMB')
+module = config_value(config, 'MODULE').__name__
+key = config_value(config, 'KEY')
 
 urlpatterns = patterns('',
-    (r'^$', 'satchmo_payment_dumb.views.pay_ship_info',
-        {'SSL':config.SSL.value}, 'DUMB_satchmo_checkout-step2'),
-    (r'^confirm/$', 'satchmo_payment_dumb.views.confirm_info',
-        {'SSL':config.SSL.value}, 'DUMB_satchmo_checkout-step3'),
+    (r'^$', '%s.views.pay_ship_info' % module,
+        {'SSL':config.SSL.value}, '%s_satchmo_checkout-step2' % key),
+    (r'^confirm/$', '%s.views.confirm_info' % module,
+        {'SSL':config.SSL.value}, '%s_satchmo_checkout-step3' % key),
     (r'^success/$', 'payment.views.checkout.success',
-        {'SSL':config.SSL.value}, 'DUMB_satchmo_checkout-success'),
+        {'SSL':config.SSL.value}, '%s_satchmo_checkout-success' % key),
 )
